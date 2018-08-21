fec079cb943a946c8959a4ebbbe567e32b0e1ff2
Anderson Cruz <ac.andersoncruz@gmail.com>
2018-08-20 22:56:21 -0400

    Ajustes Componentes PanelFeedbackSaldo

--- src/components/PanelFeedbackSaldo/index.js
+++ src/components/PanelFeedbackSaldo/index.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
-import {Icon, Box, Msg, Saldo, Cifra } from './styled'
+import {Icon, Box, Msg, Saldo, Cifra, ScrollNavigator, NavigatorContent } from './styled'
 import { card} from '../../assets/panelFeedback'
 
 class PanelFeedbackSaldo extends Component{
 
+  state = {
+    boxItems: [
+      {
+        msg1: 'Você não fez nenhuma compra nos últimos 15 dias.',
+        unidade: 'R$',
+        saldo: '886,34',
+        msg2: 'Aguardando compra',
+        source: card
+      },
+      {
+        msg1: 'Compras',
+        unidade: 'L',
+        saldo: '1.006,34',
+        msg2: 'Devendo nao sei quantos reais, vamos ver quando vai pagar',
+        source: card
+      },
+    ]
+  }
+
+  renderBoxItems = ({msg1, unidade, saldo, msg2, source}, index) => (
+    <Box key={`${index}`}>
+      <Icon height={65} width={65} source={source}/>
+      <Msg> {msg1} </Msg>
+      <Cifra>{unidade}{' '}<Saldo>{saldo}</Saldo></Cifra>
+      <Msg>{msg2}</Msg>
+    </Box>
+  )
+
   render(){
+      const {boxItems} = this.state
       return(
-          <Box>
-            <Icon height={65} width={65} source={card}/>
-            <Msg> Você não fez nenhuma compra nos últimos 15 dias.</Msg>
-            <Cifra>R${' '}<Saldo>886,34</Saldo></Cifra>
-            <Msg>Aguardando compra</Msg>
-          </Box>
+          <NavigatorContent>
+            <ScrollNavigator
+              horizontal
+              showsHorizontalScrollIndicator={true}>
+              {boxItems.map(this.renderBoxItems)}
+          </ScrollNavigator>
+        </NavigatorContent>
       )
   }
 }
--- src/components/PanelFeedbackSaldo/styled.js
+++ src/components/PanelFeedbackSaldo/styled.js
@@ -4,10 +4,27 @@ export const Icon = styled.Image`
   height: ${({ height }) => height};
   width: ${({ width }) => width};
 `
+export const ScrollNavigator = styled.ScrollView.attrs({
+  contentContainerStyle: () => ({
+    alignItems: 'center',
+    justifyContent: 'center',
+  }),
+})`
+  height: 300;
+  background-color: rgb(2, 77, 165));
+  width: 100%;
+`
+export const NavigatorContent = styled.View`
+  width: auto;
+  height: auto;
+`
+
+
 export const Box = styled.View`
   background-color: rgb(230,230,230);
-  height: 280;
-  width: 85%;
+  height: 230;
+${'' /* width: 45%; */}
+  width: 250;
   margin-left: 25;
   margin-right: 10;
   border-radius: 3;
